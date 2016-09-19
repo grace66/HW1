b563152c3e5dd33887322c0211bb33448333b7df
grace66 <mhuang1@msudenver.edu>
2016-09-18 21:45:33 -0600

    Add files via upload

---
+++ Chp1212.java
@@ -0,0 +1,28 @@
+ //question: for 1.2.12
+import java.util.Scanner;
+
+ public class Chp1212{
+    
+ public static void main(String[] args) {
+  
+  public static String dayOfTheWeek (int year, int month, int date) {
+
+ Calendar cal = Calendar.getInstance();
+        cal.set(Year, Month, Day);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+
+        System.out.println(weekDays.get(dayOfWeek));
+        {
+
+String [] DayOfWeek = new String [] {"Sunday",
+                                     "Monday", 
+                                     "Tuesday" ,
+                                     "Wednesday",
+				                         "Thursday" , 
+                                     "Friday" , 
+                                     "Saturday",};
+return DayOfWeek[calndr.get(Calendar.DAY_OF_WEEK) - 1];
+}
+
+
+
