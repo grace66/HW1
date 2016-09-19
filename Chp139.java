456b232dc94c3e897eb613121cd01d395ecab105
grace66 <mhuang1@msudenver.edu>
2016-09-18 22:01:52 -0600

    Add files via upload

---
+++ Chp139.java
@@ -0,0 +1,51 @@
+//For page 162, question #1.3.9
+public class Chp139 {
+
+  
+ 
+        public static void main(String[] args)
+        {
+               
+                Stack ops = new Stack();
+                Stack exp = new Stack();
+                
+                System.out.println("Enter number with parentheses");
+
+
+                 
+                for (int i = 0; i < line.length(); i++)
+                {
+                        switch (line.charAt(i))
+                        {
+                                case '+':
+                                case '-':
+                                case '*':                                        
+                                operators.push(line.charAt(i));
+                                break;
+                                case '0':
+                                case '1':
+                                case '2':
+                                case '3':
+                                case '4':
+                                case '5':
+                                case '6':
+                                String s = "";
+                                s += line.charAt(i);
+                                operands.push(s);
+                                break;
+                                case ')':
+                                }                                                                                    
+                 System.out.println("Enter number with parentheses");
+                 
+                  } // End output
+                  } // End class
+     
+                                  
+                       
+                                      
+
+
+
+   
+   
+   
